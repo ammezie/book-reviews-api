067c456e97c6019e9db3647bedd44e16a0368648
Bambo <bambo@prayerbox.co>
2019-11-15 19:16:49 +0100

    Increased expires_in TTL

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 60000
         ]);
     }
 
